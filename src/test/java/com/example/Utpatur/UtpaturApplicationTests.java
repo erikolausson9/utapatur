253741d5b9e4b36135c07484e7d728dd8b8771c6
erikolausson <erik@erikolausson.se>
2020-01-07 10:18:22 +0100

    Started spring boot project

---
+++ src/test/java/com/example/Utpatur/UtpaturApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Utpatur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtpaturApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
