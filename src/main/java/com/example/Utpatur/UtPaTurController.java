5e47a4a8f6709a252c1bcdc93368064285c95816
feliciahaggqvist <felicia.haggkvist@hotmail.com>
2020-01-09 16:26:49 +0100

    klar med styling min profil

--- src/main/java/com/example/Utpatur/UtPaTurController.java
+++ src/main/java/com/example/Utpatur/UtPaTurController.java
@@ -2,7 +2,9 @@ package com.example.Utpatur;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UtPaTurController {
@@ -24,7 +26,8 @@ public class UtPaTurController {
         }
 
         @GetMapping("/profil")
-        String profil() {
+        String profil(Model model) {
+
         return "profile";
         }
 
