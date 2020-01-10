3a8bc7892c9bc928bf77d6d9aaf1fd007043c11c
feliciahaggqvist <felicia.haggkvist@hotmail.com>
2020-01-10 10:41:40 +0100

    Påbörjat skapandet av Servicelagret, skapat getmapping + postmapping för medlemsregistrering

---
+++ src/main/java/com/example/Utpatur/ServiceLayer.java
@@ -0,0 +1,16 @@
+package com.example.Utpatur;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ServiceLayer {
+    @Autowired
+    DbRepository dbRepository;
+
+    public void addMember (Member member) {
+        dbRepository.addMember(member);
+    }
+
+    public Member getMember (String memberName) {
+        return dbRepository.getMemberByMemberName(memberName);
+    }
+}
--- src/main/java/com/example/Utpatur/UtPaTurController.java
+++ src/main/java/com/example/Utpatur/UtPaTurController.java
@@ -2,18 +2,26 @@ package com.example.Utpatur;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UtPaTurController {
 
     @Autowired
     DbRepository dbRepository;
+
+    @Autowired
+    ServiceLayer serviceLayer;
 
     @GetMapping("/")
     String startsida() {
@@ -32,6 +40,38 @@ public class UtPaTurController {
         String profil(Model model) {
 
         return "profile";
+        }
+
+        @GetMapping("/addMember")
+        String addMember(Model model) {
+        model.addAttribute("member", new Member());
+
+        return "member-registration";
+        }
+
+        @PostMapping("/addMember")
+        String addMember(HttpSession session, Model model, @Valid Member member, BindingResult result) {
+        if (result.hasErrors()) {
+            return "member-registration";
+        }
+
+        if(serviceLayer.getMember(member.getMemberName()) != null) {
+            model.addAttribute("memberExists", true);
+            return "addMember";
+        }
+        model.addAttribute("member", member);
+        serviceLayer.addMember(member);
+
+        List<Member> members = (List<Member>)session.getAttribute("members");
+        if (members == null) {
+            members = new ArrayList<>();
+            session.setAttribute("members", members);
+            }
+
+        members.add(member);
+        model.addAttribute("noErrors", true);
+
+        return "member-registration";
         }
 
 
