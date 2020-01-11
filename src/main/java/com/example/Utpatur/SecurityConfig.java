3da823f37994d0b030121243c0e9b85bd5532e9a
Valentina Wali <mail@valentinawali.com>
2020-01-11 14:49:13 +0100

    fixed map alignment on index.html

--- src/main/java/com/example/Utpatur/SecurityConfig.java
+++ src/main/java/com/example/Utpatur/SecurityConfig.java
@@ -30,8 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 */
 
                 .authorizeRequests()
-                    .antMatchers("/", "/kartvy", "/registrera-konto", "/tur", "/style.css", "/index.js", "/map-route.js", "/mapview.js").permitAll()
-                .antMatchers("/static/**").permitAll()
+                    .antMatchers("/", "/kartvy", "/registrera-konto", "/tur", "/style.css", "/index.js", "/map-route.js", "/mapview.js", "/images/**").permitAll()
                     .antMatchers("/profil", "/skapa-ny-tur").hasRole("USER")
                 .anyRequest().authenticated()
 
